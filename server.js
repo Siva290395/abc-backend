c756588c19e8bc1d84c8a732f860b07727ff341f
Siva290395 <129871285+Siva290395@users.noreply.github.com>
2026-04-02 13:12:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
- res.send("siva succesfully Running backend Successfully")
+ res.send("kavin Running backend Successfully")
 })
 
 app.listen(5000)
